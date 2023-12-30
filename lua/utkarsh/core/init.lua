ed675c754f4ca3552cd0ace30896b5f35bdabbec
chgkunm <coolhologramkid@gmail.com>
2023-12-31 00:01:09 +0530

    Made some changes in the config and added new plugin

--- lua/utkarsh/core/init.lua
+++ lua/utkarsh/core/init.lua
@@ -1,57 +1,57 @@
 local options = {
-  backup = false,                          -- creates a backup file
-  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
-  cmdheight = 4,                           -- more space in the neovim command line for displaying messages
-  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-  conceallevel = 0,                        -- so that `` is visible in markdown files
-  fileencoding = "utf-8",                  -- the encoding written to a file
-  hlsearch = true,                         -- highlight all matches on previous search pattern
-  ignorecase = true,                       -- ignore case in search patterns
-  mouse = "a",                             -- allow the mouse to be used in neovim
-  pumheight = 20,                          -- pop up menu height
-  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
-  showtabline = 4,                         -- always show tabs
-  smartcase = true,                        -- smart case
-  smartindent = true,                      -- make indenting smarter again
-  splitbelow = true,                       -- force all horizontal splits to go below current window
-  splitright = true,                       -- force all vertical splits to go to the right of current window
-  swapfile = false,                        -- creates a swapfile
-  termguicolors = true,                    -- set term gui colors (most terminals support this)
-  background = "dark",                     -- colorschemes that can be light or dark will be made dark
+	backup = false, -- creates a backup file
+	clipboard = "unnamedplus", -- allows neovim to access the system clipboard
+	cmdheight = 4, -- more space in the neovim command line for displaying messages
+	completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+	conceallevel = 0, -- so that `` is visible in markdown files
+	fileencoding = "utf-8", -- the encoding written to a file
+	hlsearch = true, -- highlight all matches on previous search pattern
+	ignorecase = true, -- ignore case in search patterns
+	mouse = "a", -- allow the mouse to be used in neovim
+	pumheight = 20, -- pop up menu height
+	showmode = false, -- we don't need to see things like -- INSERT -- anymore
+	showtabline = 4, -- always show tabs
+	smartcase = true, -- smart case
+	smartindent = true, -- make indenting smarter again
+	splitbelow = true, -- force all horizontal splits to go below current window
+	splitright = true, -- force all vertical splits to go to the right of current window
+	swapfile = false, -- creates a swapfile
+	termguicolors = true, -- set term gui colors (most terminals support this)
+	background = "dark", -- colorschemes that can be light or dark will be made dark
+	colorcolumn = "100", -- displays a line to represent a limit for horizontal code
 
-  timeoutlen = 300,                        -- time to wait for a mapped sequence to complete (in milliseconds)
-  undofile = true,                         -- enable persistent undo
-  updatetime = 300,                        -- faster completion (4000ms default)
-  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  expandtab = true,                        -- convert tabs to spaces
-  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
-  tabstop = 2,                             -- insert 4 spaces for a tab
-  cursorline = true,                       -- highlight the current line
-  number = true,                           -- set numbered lines
-  relativenumber = true,                   -- set relative numbered lines
-  numberwidth = 2,                         -- set number column width to 2 {default 4}
+	timeoutlen = 300, -- time to wait for a mapped sequence to complete (in milliseconds)
+	undofile = true, -- enable persistent undo
+	updatetime = 300, -- faster completion (4000ms default)
+	writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+	expandtab = true, -- convert tabs to spaces
+	shiftwidth = 2, -- the number of spaces inserted for each indentation
+	tabstop = 2, -- insert 4 spaces for a tab
+	cursorline = true, -- highlight the current line
+	number = true, -- set numbered lines
+	relativenumber = true, -- set relative numbered lines
+	numberwidth = 2, -- set number column width to 2 {default 4}
 
-  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
-  wrap = false,                            -- display lines as one long line
-  linebreak = true,                        -- companion to wrap, don't split words
-  scrolloff = 8,                           -- minimal number of screen lines to keep above and below the cursor
-  sidescrolloff = 8,                       -- minimal number of screen columns either side of cursor if wrap is `false`
---  guifont = "monospace:h17",             -- the font used in graphical neovim applications
-  whichwrap = "bs<>[]hl",                  -- which "horizontal" keys are allowed to travel to prev/next line
-  
+	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+	wrap = false, -- display lines as one long line
+	linebreak = true, -- companion to wrap, don't split words
+	scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor
+	sidescrolloff = 8, -- minimal number of screen columns either side of cursor if wrap is `false`
+	--  guifont = "monospace:h17",             -- the font used in graphical neovim applications
+	whichwrap = "bs<>[]hl", -- which "horizontal" keys are allowed to travel to prev/next line
 }
 
 for k, v in pairs(options) do
-  vim.opt[k] = v
+	vim.opt[k] = v
 end
 
 -- vim.opt.shortmess = "ilmnrx"                        -- flags to shorten vim messages, see :help 'shortmess'
-vim.opt.shortmess:append "c"                           -- don't give |ins-completion-menu| messages
-vim.opt.iskeyword:append "-"                           -- hyphenated words recognized by searches
-vim.opt.formatoptions:remove({ "c", "r", "o" })        -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
-vim.opt.runtimepath:remove("/usr/share/vim/vimfiles")  -- separate vim plugins from neovim in case vim still in use
+vim.opt.shortmess:append("c") -- don't give |ins-completion-menu| messages
+vim.opt.iskeyword:append("-") -- hyphenated words recognized by searches
+vim.opt.formatoptions:remove({ "c", "r", "o" }) -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
+vim.opt.runtimepath:remove("/usr/share/vim/vimfiles") -- separate vim plugins from neovim in case vim still in use
 
---****************************** 
+--******************************
 -- keymaps start here
 
 local opts = { noremap = true, silent = true }
@@ -71,23 +71,20 @@ local keymap = vim.keymap.set
 --   command_mode = "c",
 
 -- Keymaps for better default experience
-keymap({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+keymap({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Remap for dealing with word wrap
-keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-keymap('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-keymap('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-keymap('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-keymap('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+keymap("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+keymap("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+keymap("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+keymap("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 -- save
-keymap("n", "<leader>w", ":w<CR>", opts)
-
--- save
-keymap("n", "<leader>q", ":q<CR>", opts)
+keymap("n", "<leader>w", ":w<CR>", opts, { desc = "save the current file" })
 
 -- save and quit
 keymap("n", "wq", ":wq<CR>", opts)
@@ -96,10 +93,10 @@ keymap("n", "wq", ":wq<CR>", opts)
 keymap("n", "qq", ":wqa<CR>", opts)
 
 -- move to end of line
-keymap("n", "0", "$", opts)
+keymap({ "n", "v" }, "0", "$", opts, { desc = "end of the line" })
 
 -- move to start of line
-keymap("n", "1", "0", opts)
+keymap({ "n", "v" }, "1", "0", opts, { desc = "start of the line" })
 
 -- Normal --
 -- clear search highlights
@@ -128,12 +125,12 @@ keymap("n", "<A-j>", ":m .+1<CR>==", opts)
 keymap("n", "<A-k>", ":m .-2<CR>==", opts)
 
 -- Insert --
--- Press jk fast to exit insert mode 
+-- Press jk fast to exit insert mode
 keymap("i", "jk", "<ESC>", opts)
 keymap("i", "kj", "<ESC>", opts)
 
 -- Visual --
--- Press jk fast to exit visual mode 
+-- Press jk fast to exit visual mode
 keymap("v", "kj", "<ESC>", opts)
 keymap("v", "jk", "<ESC>", opts)
 
@@ -161,12 +158,11 @@ keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 -- set system clipboard
-keymap('n','y','"+y')
-keymap('n','yy','"+yy')
-keymap('n','Y','"+Y')
-keymap('x','y','"+y')
-keymap('x','Y','"+Y')
-
+keymap("n", "y", '"+y')
+keymap("n", "yy", '"+yy')
+keymap("n", "Y", '"+Y')
+keymap("x", "y", '"+y')
+keymap("x", "Y", '"+Y')
 
 -- window management
 keymap("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
--- lua/utkarsh/plugins/treesitter.lua
+++ lua/utkarsh/plugins/treesitter.lua
@@ -1,20 +1,28 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        ensure_installed = {
-          "lua",
-          "javascript",
-          "typescript",
-          "html",
-          "css",
-        },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				ensure_installed = {
+					"c",
+					"cpp",
+					"go",
+					"lua",
+					"python",
+					"rust",
+					"tsx",
+					"javascript",
+					"typescript",
+					"vimdoc",
+					"vim",
+					"bash",
+				},
+
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
